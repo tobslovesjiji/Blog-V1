5d41075bfbf71419416aa8e31d5ea855b9b39c18
tobslovesjiji <21150@my.sanctamaria.school.nz>
2022-08-12 12:20:51 +1200

    footer and more

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 
 #allows you to reset your password
+#need to find an available mail server for it to function
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0a03872ea9d2848cb5e66bf7f4e3cdfa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -13,11 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'outlook.com'
+app.config['MAIL_SERVER'] = ''
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = ''
 app.config['MAIL_PASSWORD'] = ''
 mail = Mail(app)
 
+#this needs to be at the bottom instead of the top, as everything else needs to be initiated before we can import routes
 from main_app import routes
